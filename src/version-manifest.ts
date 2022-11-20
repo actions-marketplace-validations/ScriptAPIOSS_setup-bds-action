5abf32114cc34534d37e78f11db4998a1554fac6
GTB3NW <GTB3NW@users.noreply.github.com>
2022-11-20 19:14:52 +0000

    formatting

--- src/version-manifest.ts
+++ src/version-manifest.ts
@@ -1,11 +1,11 @@
 export interface VersionManifest {
-  commit_hash: string;
-  build_id: string;
-  date: Date;
-  version: string;
-  platform: string;
-  sha1: string;
-  size_in_bytes: number;
-  download_url: string;
-  release_notes: string;
+  commit_hash: string
+  build_id: string
+  date: Date
+  version: string
+  platform: string
+  sha1: string
+  size_in_bytes: number
+  download_url: string
+  release_notes: string
 }
--- src/versions.ts
+++ src/versions.ts
@@ -1,18 +1,18 @@
 export interface Linux {
-  stable: string;
-  preview: string;
-  versions: string[];
-  preview_versions: string[];
+  stable: string
+  preview: string
+  versions: string[]
+  preview_versions: string[]
 }
 
 export interface Windows {
-  stable: string;
-  preview: string;
-  versions: string[];
-  preview_versions: string[];
+  stable: string
+  preview: string
+  versions: string[]
+  preview_versions: string[]
 }
 
 export interface VersionsJson {
-  linux: Linux;
-  windows: Windows;
+  linux: Linux
+  windows: Windows
 }
