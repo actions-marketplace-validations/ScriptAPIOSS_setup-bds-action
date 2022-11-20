882a3ffdaa56b65edd3adcd37a3a0d0bc0887a95
GTB3NW <GTB3NW@users.noreply.github.com>
2022-11-20 18:06:19 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,110 @@
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as http from '@actions/http-client'
+import { VersionsJson } from './versions';
+import { VersionManifest } from './version-manifest';
+
+const VERSIONS_URL = "https://raw.githubusercontent.com/Bedrock-OSS/BDS-Versions/main/versions.json";
+
+async function run(): Promise<void> {
+  try {
+
+    switch (process.platform) {
+      case "win32": { throw new Error("Unsupported platform: ${process.platform}") };
+      case "darwin": { throw new Error("Unsupported platform: ${process.platform}") };
+    }
+
+    let BDS_VERSION: string = core.getInput('BDS_VERSION');
+    const BDS_CHANNEL: string = core.getInput('BDS_CHANNEL');
+
+    if (BDS_VERSION === undefined || BDS_VERSION === "") {
+      BDS_VERSION = "latest";
+    }
+
+    const USE_LATEST = BDS_VERSION === "latest";
+    const USE_PREVIEW = BDS_CHANNEL === "preview";
+
+    if (USE_LATEST) {
+      core.debug("Using latest")
+    }
+
+    // Validate channel
+    switch (BDS_CHANNEL) {
+      case "stable": { break };
+      case "preview": { break };
+      default: {
+        throw new Error(`Invalid BDS_CHANNEL: ${BDS_CHANNEL}`);
+      }
+    }
+
+    const _http = new http.HttpClient('http-client-tests');
+
+    core.debug(`Pulling versions.json for latest version info`);
+
+    const versions_response: http.HttpClientResponse = await _http.get(
+      VERSIONS_URL
+    );
+
+    if (versions_response.message.statusCode !== 200) {
+      throw new Error(`Bad response code while pulling versions.json: ${versions_response.message.statusCode}`);
+    }
+
+    const version_response_body: string = await versions_response.readBody();
+    const versions: VersionsJson = JSON.parse(version_response_body);
+
+    let target_version = BDS_VERSION;
+
+    // Sanity check version exists
+    if (USE_LATEST) {
+      if (USE_PREVIEW) {
+        target_version = versions.linux.preview;
+      } else {
+        target_version = versions.linux.stable;
+      }
+    } else {
+      if (USE_PREVIEW) {
+        if (!versions.linux.preview_versions.includes(target_version)) {
+          throw new Error(`Unknown version for channel: ${target_version} [${BDS_CHANNEL}]`);
+        }
+      } else {
+        if (!versions.linux.versions.includes(target_version)) {
+          throw new Error(`Unknown version for channel: ${target_version} [${BDS_CHANNEL}]`);
+        }
+      }
+    }
+
+    core.debug(`Downloading bds version ${target_version} on channel ${BDS_CHANNEL}`);
+
+    let version_url = "https://raw.githubusercontent.com/Bedrock-OSS/BDS-Versions/main/linux";
+
+    if (USE_PREVIEW) {
+      version_url += "_preview"
+    }
+
+    version_url += `/${target_version}.json`
+
+    const version_manifest_response: http.HttpClientResponse = await _http.get(
+      version_url
+    );
+
+    if (version_manifest_response.message.statusCode !== 200) {
+      throw new Error(`Bad response code while pulling version manifest: ${versions_response.message.statusCode}`);
+    }
+
+    const version_manifest_response_body: string = await version_manifest_response.readBody();
+    const versions_manifest: VersionManifest = JSON.parse(version_manifest_response_body);
+
+    const bds_zip = await tc.downloadTool(versions_manifest.download_url);
+    const bdsExtractedPath = await tc.extractZip(bds_zip, './bds');
+
+    const bdsCachedPath = await tc.cacheDir(bdsExtractedPath, 'bds', target_version);
+    core.addPath(bdsCachedPath);
+
+    core.summary.addHeading("Test summary");
+
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
---
+++ src/versions.ts
@@ -0,0 +1,18 @@
+export interface Linux {
+  stable: string;
+  preview: string;
+  versions: string[];
+  preview_versions: string[];
+}
+
+export interface Windows {
+  stable: string;
+  preview: string;
+  versions: string[];
+  preview_versions: string[];
+}
+
+export interface VersionsJson {
+  linux: Linux;
+  windows: Windows;
+}
